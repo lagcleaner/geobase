77cae4f284b5881d9238d29c34fb32253379c660
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2021-05-23 23:58:23 -0400

    Refactor Home page widget

--- lib/src/presentation/pages/home/home_page.dart
+++ lib/src/presentation/pages/home/home_page.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/plugin_api.dart';
-import 'package:latlong2/latlong.dart';
 
-class CachedTileProvider extends TileProvider {
-  const CachedTileProvider();
-
-  @override
-  ImageProvider getImage(Coords<num> coords, TileLayerOptions options) {
-    return NetworkImage(getTileUrl(coords, options));
-  }
-}
+import '../../../core/constants/constants.dart';
+import 'widgets/map/map.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({
@@ -20,32 +12,11 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('GeoBase'),
+        title: const Text(APP_NAME),
       ),
-      body: FlutterMap(
-        options: MapOptions(
-          // bounds: LatLngBounds(LatLng(23.11, -82.35), LatLng(23.0, -82.34)),
-          boundsOptions: const FitBoundsOptions(padding: EdgeInsets.all(8.0)),
-          center: LatLng(23.11, -82.35),
-        ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: ['a', 'b', 'c'],
-            tileProvider: const CachedTileProvider(),
-          ),
-          MarkerLayerOptions(
-            markers: [
-              Marker(
-                width: 50.0,
-                height: 50.0,
-                point: LatLng(23.11, -82.35),
-                builder: (ctx) => Container(
-                  color: Colors.black.withOpacity(0.3),
-                ),
-              ),
-            ],
-          ),
+      body: Stack(
+        children: [
+          const GeoBaseMap(),
         ],
       ),
     );
