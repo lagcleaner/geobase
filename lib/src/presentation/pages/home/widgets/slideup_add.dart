795f4db22bd2d59acc87c462306eea3ce54cc1b0
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2021-05-23 23:57:29 -0400

    Slideup base widgets for slide up panel

---
+++ lib/src/presentation/pages/home/widgets/slideup_add.dart
@@ -0,0 +1,9 @@
+import 'package:geobase/app.dart';
+
+class SlideUpAddPointBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
