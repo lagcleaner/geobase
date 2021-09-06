e4ca92a6ddfbf6bae93fb4a1e4080f8d39ae58dc
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2021-09-06 02:19:45 -0400

    Update dartz package

--- lib/src/presentation/core/failures/failures.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'failures.freezed.dart';
-
-@freezed
-abstract class Failure with _$Failure {
-  const factory Failure.connection() = _ConnectionFailure;
-}
--- lib/src/presentation/views/home/blocs/marker/marker_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'marker_bloc.dart';
-
-@freezed
-class MarkerEvent with _$MarkerEvent {
-  const factory MarkerEvent.started([LatLng? corner1, LatLng? corner2]) =
-      _MarkerStarted;
-  const factory MarkerEvent.moved(LatLng corner1, LatLng corner2) = _MapMoved;
-}
