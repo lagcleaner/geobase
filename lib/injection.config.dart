9dabdc121bc7ff39945cb44c944db9f37c9dc433
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2021-09-09 03:41:16 -0400

    Injectable files updated

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -7,23 +7,31 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import 'src/domain/usecases/configurations/load_source_configuration.dart'
-    as _i4;
-import 'src/domain/usecases/configurations/load_user_preferences.dart' as _i5;
-import 'src/domain/usecases/map/get_markers.dart' as _i3;
+import 'src/domain/services/configurations/configuration_service.dart' as _i8;
+import 'src/domain/services/configurations/interfaces/i_map_source_conf_reader.dart'
+    as _i7;
+import 'src/domain/services/map/filters_service.dart' as _i6;
+import 'src/domain/services/map/interfaces/i_filters.dart' as _i5;
+import 'src/domain/services/map/interfaces/i_location.dart' as _i3;
+import 'src/domain/services/map/interfaces/i_markers.dart' as _i9;
+import 'src/domain/services/map/location_service.dart' as _i4;
+import 'src/domain/services/map/markers_service.dart' as _i10;
 import 'src/presentation/views/home/blocs/map/map_cubit.dart'
-    as _i6; // ignore_for_file: unnecessary_lambdas
+    as _i11; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  gh.lazySingleton<_i3.GetMarkers>(() => _i3.GetMarkers());
-  gh.lazySingleton<_i4.LoadSourceConfiguration>(
-      () => _i4.LoadSourceConfiguration());
-  gh.lazySingleton<_i5.LoadUserPreferences>(() => _i5.LoadUserPreferences());
-  gh.factory<_i6.MapCubit>(
-      () => _i6.MapCubit(loadSourceConfiguration: get<dynamic>()));
+  gh.lazySingleton<_i3.ILocationConfigurationService>(
+      () => _i4.GetCurrentLocation());
+  gh.lazySingleton<_i5.IMapFiltersOptionsLoaderService>(
+      () => _i6.MapFilterService());
+  gh.lazySingleton<_i7.IMapSourceConfigurationChangesListenerService>(
+      () => _i8.ConfigurationService());
+  gh.lazySingleton<_i9.IMarkerGetterService>(() => _i10.GetMarkers());
+  gh.factory<_i11.MapCubit>(() => _i11.MapCubit(
+      sourceConfReader: get<_i7.IMapSourceConfigurationReaderService>()));
   return get;
 }
---
+++ lib/src/domain/services/services.dart
@@ -0,0 +1,7 @@
+export 'configurations/interfaces/i_map_source_conf_reader.dart';
+export 'configurations/interfaces/i_map_source_conf_writter.dart';
+export 'configurations/interfaces/i_user_preferences_reader.dart';
+export 'configurations/interfaces/i_user_preferences_writter.dart';
+export 'map/interfaces/i_filters.dart';
+export 'map/interfaces/i_location.dart';
+export 'map/interfaces/i_markers.dart';
